137e0c608831eb8045488e60d6037188196d7413
matheusjagi <jagi.matheus@gmail.com>
2022-11-23 16:12:30 -0300

    initial commit

---
+++ user-service/src/test/java/com/picpay/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
