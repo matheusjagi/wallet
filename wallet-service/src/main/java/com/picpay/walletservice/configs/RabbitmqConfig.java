7fcb54bb0d440b9f83a36ed5cd5ef06569efa33a
matheusjagi <jagi.matheus@gmail.com>
2022-11-26 00:31:06 -0300

    basic implementation and initial setup of asynchronous calls with rabbitmq

---
+++ wallet-service/src/main/java/com/picpay/walletservice/configs/RabbitmqConfig.java
@@ -0,0 +1,41 @@
+package com.picpay.walletservice.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    @Autowired
+    CachingConnectionFactory cachingConnectionFactory;
+
+//    @Value("${picpay.broker.exchange.}")
+//    private String exchangeWalletEvent;
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(cachingConnectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+//    @Bean
+//    public FanoutExchange fanoutExchange() {
+//        return new FanoutExchange(exchangeWalletEvent);
+//    }
+}
